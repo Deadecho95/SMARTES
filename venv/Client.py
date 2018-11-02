ed3ac2b81008a0ebcced155de58254922922ec5c
Deadecho95 <43399859+Deadecho95@users.noreply.github.com>
2018-11-02 11:26:55 +0100

    first code

--- venv/Client.py
+++ venv/Client.py
@@ -1,23 +1,26 @@
-#---------------------------------------------------------------------------#
+# --------------------------------------------------------------------------- #
 # import the various server implementations
-#---------------------------------------------------------------------------#
-import pymodbus
-from pymodbus.client.sync import ModbusTcpClient
+# --------------------------------------------------------------------------- #
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
-#---------------------------------------------------------------------------#
+
+
+# --------------------------------------------------------------------------- #
 # configure the client logging
-#---------------------------------------------------------------------------#
+# --------------------------------------------------------------------------- #
 import logging
-
-from pymodbus.server.sync import ModbusTcpServer
-
-logging.basicConfig()
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-client = ModbusTcpClient("127.0.0.1")
+UNIT = 0x1
+
+client = ModbusClient('localhost', port=5020)
 client.connect()
-client.write_coil(1, True)
-result = server.read_coils(1,1)
+
+client.write_coil(1, False)
+result = client.read_coils(1,1)
 print(result.bits[0])
 client.close()
