98333ca5a639348ed07bf93cba7c0e0f9567188e
Deadecho95 <43399859+Deadecho95@users.noreply.github.com>
2018-11-09 09:19:19 +0100

    Restart

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\chena\PycharmProjects\SMARTES\venv"
+$env:VIRTUAL_ENV="C:\Users\Johan\Documents\GitHub\SMARTES\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
