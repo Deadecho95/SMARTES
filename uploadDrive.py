c5f1f1e1ccc30053ca62913532a5411975a8e5e5
Nassim <nassim.augsburger@students.hevs.ch>
2018-11-09 16:09:49 +0100

    Controller

--- uploadDrive.py
+++ uploadDrive.py
@@ -10,7 +10,11 @@ file1 = drive.CreateFile({'title': 'Hello.txt'})  # Create GoogleDriveFile insta
 
 file1.SetContentString('Hello World!') # Set content of the file from given string.
 file1.Upload()
+
+
 # Auto-iterate through all files that matches this query
+
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file1 in file_list:
+
     print('title: %s, id: %s' % (file1['title'], file1['id']))
