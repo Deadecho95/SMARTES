69216cacc58095ef9a5da3a4f4731a764cdc9fca
Deadecho95 <43399859+Deadecho95@users.noreply.github.com>
2018-11-09 16:07:06 +0100

    Controller

--- Controller.py
+++ Controller.py
@@ -1,4 +1,5 @@
-
+from ClientModBus import ClientModBus
+from threading import Timer
 
 # --------------------------------------------------------------------------- #
 # the controller
@@ -12,8 +13,29 @@ class Controller:
         """ Initialize
         """
 
-def connect(self):
-    """ connect to ServerModBus
-    """
 
-    self.clientVenus.connect()
+    def transmittValue(self):
+        """
+        transmitt the values
+        :return:
+        """
+
+    def getsModbusValues(self):
+        """
+        get value from th modbus
+        :return:
+        """
+        client1 = ClientModBus(0x1,"153.109.7.29")
+        client1.connect()
+        self.data = client1.get_registers()
+        client1.disconnect()
+
+    def processingValues(self):
+        """
+        traitement of the vlues for the cloud
+        :return:
+        """
+        for i in range(0, self.data.lenght()):
+            for y in range(0, self.data[i].lenght()):
+                value = self.data[i][y]
+
