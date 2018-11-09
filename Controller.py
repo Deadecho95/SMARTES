fd1b4ce7c78ebda31767ca835f4e0c22f3ea2037
Deadecho95 <43399859+Deadecho95@users.noreply.github.com>
2018-11-09 13:32:18 +0100

    image

---
+++ Controller.py
@@ -0,0 +1,19 @@
+
+
+# --------------------------------------------------------------------------- #
+# the controller
+# --------------------------------------------------------------------------- #
+
+
+class Controller:
+    """ Implementation of a client
+    """
+    def __init__(self):
+        """ Initialize
+        """
+
+def connect(self):
+    """ connect to ServerModBus
+    """
+
+    self.clientVenus.connect()
