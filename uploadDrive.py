fc2e2f7a2ee0b601ac40c842694fd0bfb16f3d09
Nassim <nassim.augsburger@students.hevs.ch>
2018-11-16 13:16:18 +0100

    Controller

--- uploadDrive.py
+++ uploadDrive.py
@@ -1,8 +1,7 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import datetime
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+
 
 # --------------------------------------------------------------------------- #
 # Client to connect to Google Drive
@@ -11,36 +10,38 @@ gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentica
 
 class UploadDrive:
 
-    def __init__(self,drive):
+    def __init__(self):
         """ Initialize a client instance
 
-    :param UNIT: The host to connect to (default 0x1)
-    :param address: The tcp address to connect to (default localhost)
-    :param port: The modbus port to connect to (default 502)
+    :param drive: The Drive connected
     """
 
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
         self.drive = GoogleDrive(gauth)
 
-    def __send__(self,text='hello',titleText='a'):
+    def send(self, text='hello', title='a'):
         """ connect to ServerModBus
         """
-
         datetime.datetime.now().now.strftime("%Y-%m-%d %H:%M")
+        file1 = self.drive.CreateFile({'title': title+'.txt'})
 
-        file1 = self.drive.CreateFile({'title': titleText+'.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+# Create GoogleDriveFile instance with title 'Hello.txt'.
 
         file1.SetContentString(text) # Set content of the file from given string.
         file1.Upload()
 
 
 # Auto-iterate through all files that matches this query
-    def __read__(self,titleText='a'):
-        b=0
+    def read(self,title='a'):
+        b = 0
+
         file_list = self.drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
         for file1 in file_list:
+            self.drive.
             print('title: %s, id: %s' % (file1['title'], file1['id']))
-            if file1['title']==titleText:
+            if file1['title'] == title:
                 print('found')
-                b+=1
-        if b==0:
+                b += 1
+        if b == 0:
             print('file not found')
