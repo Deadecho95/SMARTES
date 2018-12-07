afcfd1ea9b8922400421b9f6311c8698b9699e51
Deadecho95 <43399859+Deadecho95@users.noreply.github.com>
2018-12-07 10:08:35 +0100

    gitignore

--- Controller.py
+++ Controller.py
@@ -1,6 +1,5 @@
-from ClientModBus import ClientModBus
-from ClientModBus import ClientModBus
-from threading import Timer
+from clientModBus import ClientModBus
+from uploadDrive import UploadDrive
 
 # --------------------------------------------------------------------------- #
 # the controller
@@ -9,34 +8,70 @@ from threading import Timer
 
 class Controller:
     """ Implementation of a client
+
+    :param client: The clientModbus to connect to
+
     """
-    def __init__(self):
+
+    def __init__(self, client):
         """ Initialize
         """
+        self.name = 0
+        self.data = 0
+        self.client = client
 
-
-    def transmittValue(self):
+    def startTransmittToCloud(self):
         """
-        transmitt the values
+        start the transmission from the battery to the cloud
+        :return:
+        """
+        self.getModbusValues()
+        self.processingValues()
+        self.transmittValue()
+
+    def startTransmittToBattery(self):
+            """
+            start the transmission from the battery to the cloud
+            :return:
+            """
+
+    def getModbusValues(self):
+        """
+        get value from the modbus
+        :return:
+        """
+        self.client.connect()
+        self.data = self.client.get_registers()
+        self.client.disconnect()
+
+    def setModbusValue(self):
+        """
+        set value to the modbus
         :return:
         """
 
-    def getsModbusValues(self):
-        """
-        get value from th modbus
-        :return:
-        """
-        client1 = ClientModBus(0x1,"153.109.7.29")
-        client1.connect()
-        self.data = client1.get_registers()
-        client1.disconnect()
+        self.client.connect()
+        self.set_register("""value""")
+        self.disconnect()
 
-    def processingValues(self):
+    def setModbusValues(self):
         """
-        traitement of the vlues for the cloud
+        set values to the modbus
         :return:
         """
-        for i in range(0, self.data.lenght()):
-            for y in range(0, self.data[i].lenght()):
-                value = self.data[i].re
+
+        self.client.connect()
+        self.client.set_registers("""value""")
+        self.client.disconnect()
+
+    def WriteCloud(self):
+        """
+        transmitt registers and names to the cloud
+        :return:
+        """
+        i = 0
+        while i <= len(self.data):
+            """uploddrive(data[i],data[i+1].registers)"""   # data[i] = name // data[i+1].registers = list
+            i = i+2
+
 
