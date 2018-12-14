1a3479eaa87806e5bd8b0eca98c1771756deeb0e
Vincent <44081716+ManiChunk@users.noreply.github.com>
2018-12-14 12:59:43 +0100

    Update IO.py

--- IO/IO.py
+++ IO/IO.py
@@ -1,22 +1,68 @@
 #---------------------------------------------------------------------------#
 # import the various server implementations
 # ---------------------------------------------------------------------------#
-
-# --------------------------------------------------------------------------- #
-# Analog input
-# 4mA at 745 D/A  -  20mA at 3723 D/A
-# --------------------------------------------------------------------------- #
-from time import sleep
 from widgetlords.pi_spi import *
+import RPi.GPIO as GPIO
 
-init()
-outputs = Mod2AO()
+class In_out:
 
-while True:
-    outputs.write_single(0, 745)
-    outputs.write_single(1, 3723)
-    sleep(2)
+    def __init__(self, ):
+        """ Initialize
+        """
+        init()
+        self.outputs = Mod2AO()
+        self.inputs = Mod8DI()
 
-    outputs.write_single(0, 3733)
-    outputs.write_single(1, 745)
-    sleep(2)
+        GPIO.setmode(GPIO.BCM)
+
+        self.set_relay(37)
+        self.set_relay(38)
+        self.set_relay(40)
+
+
+
+    def set_analog_output(self, pin,value):
+        """
+        Set analog output 4-20mA
+        (4mA at 745 D/A  -  20mA at 3723 D/A)
+
+        :param pin: number of pin
+        :param value: value of output in %
+        """
+
+        self.outputs.write_single(pin, (value*29.78+745))
+
+
+    def read_digital_input(self, pin):
+        """
+        Read digital input on external card
+
+        :param pin: number of pin
+        :return: value on the pin
+        """
+        return self.inputs.read_single(pin)
+
+
+    def set_relay(self, pin):
+        """
+        Set relay in output
+
+        :param pin: the pin
+        """
+        GPIO.output(pin, GPIO.OUT)
+
+
+    def set_relay_value(self, pin, value):
+        """
+        Set value of relay
+
+        :param pin: number of pin
+        :param value: value 0 for hight and 1 for hight
+        """
+
+        if value == 0:
+            GPIO.output(pin, GPIO.LOW)
+        else:
+            GPIO.output(pin, GPIO.HIGH)
+
+
