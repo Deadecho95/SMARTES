2d969249f8d2f903105fb34b53264cc859979263
nassimaugsburg <43399765+nassimaugsburg@users.noreply.github.com>
2018-12-14 18:51:38 +0100

    test CLoud

--- Cloud/testCloud.py
+++ Cloud/testCloud.py
@@ -1,3 +1,8 @@
-import Cloud.uploadDrive
+from Cloud.uploadDrive import UploadDrive
+from pydrive.files import GoogleDriveFile
 client = UploadDrive()
+client.write_file_on_cloud("ggdd", "tttffff")
 
+fileId = client.find_file_on_cloud("ttt.txt")
+gdf =GoogleDriveFile(fileId)
+#client.delete_file_on_cloud(fileId)
